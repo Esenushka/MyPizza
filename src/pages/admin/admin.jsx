1080d6b0b3029fa91532358d48e185e14e4bcf41
Esenushka <prosterling.mien@gmail.com>
2022-01-27 17:39:55 +0600

    change auth

--- src/pages/admin/admin.jsx
+++ src/pages/admin/admin.jsx
@@ -1,16 +1,20 @@
 import css from './admin.module.css'
 import { useHistory } from 'react-router-dom'
+import { useState } from 'react';
 
 export const Admin = ()=>{
     let history = useHistory();
     const submit =(e)=>{
         e.preventDefault()
-        history.push('/adminPanel')
+        if(e.target[0].value === 'Esenushka' && e.target[1].value === 'esen2709'){
+            history.push('/adminPanel')
+        }
     }
     return(
         <div className={css.wrapper}>
             <form onSubmit={submit}>
             <div className={css.block}>
+                <h2>Admin</h2>
                <input placeholder='Login' type="text" /> 
                <input placeholder='Password' type='password'/>
                <button>LOGIN</button>
